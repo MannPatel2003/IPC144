6c148b20107d17447d71cfae7f92cfea3c8bca53
Mann Manishkumar Patel <mannp6903@gmail.com>
2022-11-05 01:10:40 -0400

    First Commit

---
+++ w1p1.c
@@ -0,0 +1,28 @@
+/*
+****************************************************************************
+                <assessment name example: Workshop - #1 (Part-1)
+Full Name   : Mann Manishkumar Patel
+Student Id# : 164282212
+Email       : mpatel462@myseneca.ca
+Section     : CPA
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not benn
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+****************************************************************************
+*/
+#include <stdio.h>
+int main()
+{
+	printf("Workshop 1 Part-1\n=================\n\nI'm displaying this using the 'printf' stdio\n(standard input output) library function!\n\n");
+	printf("Dear teacher,\n\n");
+	printf(" I promise I will work hard from this day onward.\n");
+	printf(" I acknowledge that practice is extremely important,\n");
+	printf(" so I will do all workshops, quizzes, and assignments.\n\n");
+	printf("Sincerely,\n\n");
+	printf("Mann Patel\n");
+	printf("Student ID# 164282212");
+	return 0;
+
+}
